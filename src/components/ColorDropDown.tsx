c1df769bd9684c018bdde7e25fa46d7158973892
Christian <christian980aja@gmail.com>
2026-09-25 16:50:00 +0700

    fix import filename mismatch

--- src/components/ColorDropDown.tsx
+++ src/components/ColorDropDown.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import styles from "./ColorDropdown.module.css";
+import styles from "./ColorDropDown.module.css";
 
 interface ColorDropdownProps {
   colors: string[];
